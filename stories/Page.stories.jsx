dbc88f1b9ac599d87931d153d08d674cfbe21998
Brandon Durham <brandon@smallparade.com>
2022-03-23 20:24:32 -0500

    move page to components group

--- stories/Page.stories.jsx
+++ stories/Page.stories.jsx
@@ -4,7 +4,7 @@ import { within, userEvent } from '@storybook/testing-library';
 import { Page } from './Page';
 
 export default {
-  title: 'Components by Category/Page/Examples',
+  title: 'Components/Page/Examples',
   component: Page,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
